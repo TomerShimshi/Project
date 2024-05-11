56f28894447f4d0288a1e91e5d268a2456c88d0b
Tomer Shimshi <tomershimshi@gmail.com>
2024-05-11 23:06:37 +0300

    added the eval

--- finale_project_just_eval.py
+++ finale_project_just_eval.py
@@ -1,6 +1,7 @@
 import os
 import json
 import torch
+import yaml
 from datasets import load_dataset
 from transformers import (
     AutoModelForCausalLM,
@@ -14,6 +15,7 @@ import pytest
 
 ### EVAL ###
 from deepeval import evaluate
+from deepeval.evaluate import TestResult
 from deepeval.metrics import AnswerRelevancyMetric,HallucinationMetric,ContextualPrecisionMetric
 from deepeval.metrics.ragas import RagasMetric
 from deepeval.test_case import LLMTestCase
@@ -26,10 +28,13 @@ from deepeval.test_case import LLMTestCaseParams
 import subprocess
 import csv
 from tqdm import tqdm
-def save_list_to_json(file_path: str, my_list: list):
-    with open(file_path, "w") as json_file:
-        json.dump(my_list, json_file)   
-    print(f"save the list to {file_path}")    
+
+def convert_to_serializable(data):
+    if isinstance(data, set):
+        return list(data)
+    return data
+
+
 def csv_to_dict(file_name):
     data_dict = {}
     
@@ -45,6 +50,63 @@ def csv_to_dict(file_name):
             
     
     return new_dict
+
+def test_result_to_dict(test_result: TestResult):
+    test_dict ={}
+    for metric in test_result.metrics:
+        test_dict[metric.__name__] = {}
+        
+        successful = True
+        if metric.error is not None:
+            successful = False
+        else:
+            # This try block is for user defined custom metrics,
+            # which might not handle the score == undefined case elegantly
+            try:
+                if not metric.is_successful():
+                    successful = False
+            except:
+                successful = False
+
+        #if not successful:    
+            
+        #    result =    f"  - ❌ {metric.__name__} (score: {metric.score}, threshold: {metric.threshold}, strict: {metric.strict_mode}, evaluation model: {metric.evaluation_model}, reason: {metric.reason}, error: {metric.error})"
+        #    
+        #else:
+        #    
+        #    result =   f"  - ✅ {metric.__name__} (score: {metric.score}, threshold: {metric.threshold}, strict: {metric.strict_mode}, evaluation model: {metric.evaluation_model}, reason: {metric.reason}, error: {metric.error})"
+        test_dict[metric.__name__]['is_successful']=metric.is_successful()    
+        test_dict[metric.__name__]['score']=metric.score
+        test_dict[metric.__name__]['threshold']=metric.threshold
+        test_dict[metric.__name__]['strict_mode']=metric.strict_mode
+        test_dict[metric.__name__]['evaluation_model']=metric.evaluation_model
+        test_dict[metric.__name__]['reason']=metric.reason
+        test_dict[metric.__name__]['error']=metric.error
+        
+        
+        
+        if metric.score_breakdown:
+            for metric_name, score in metric.score_breakdown.items():
+                #print(f"      - {metric_name} (score: {score})")
+                test_dict[f"{metric_name}_score"] = score #f" {metric_name} (score: {score})"
+
+    #print("")
+    #print("For test case:\n")
+    
+    test_dict['input'] = {test_result.input}
+    test_dict["actual output"] = test_result.actual_output
+    test_dict["expected output"] =test_result.expected_output
+    test_dict["context"] =  test_result.context
+    test_dict["retrieval context"] = test_result.retrieval_context
+    return test_dict
+def save_list_to_json_yaml(input_dict,base_path):
+    json_base_path = base_path.replace('yaml', 'json')
+    with open(base_path, 'w',encoding='utf-8') as outfile:
+            yaml.dump(input_dict, outfile, default_flow_style=False,indent=4)
+
+    with open(json_base_path, 'w',encoding='utf-8') as outfile:
+            json.dump(input_dict, outfile,indent=4, default=convert_to_serializable)
+    print(f"saved results to {base_path}")
 
 ### EVAL ###
 
@@ -77,85 +139,105 @@ class Mistral7B(DeepEvalBaseLLM):
     def get_model_name(self):
         return "Mistral 7B"
  
-
-compute_dtype = getattr(torch, "float16")
-quant_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=compute_dtype,
-    bnb_4bit_use_double_quant=False,
-)
-
-#######################
-### Load Eval Model ###
-#######################
-os.environ["DEEPEVAL_RESULTS_FOLDER"] = "dataset"
-model_id = "mistralai/Mistral-7B-v0.1" #"yam-peleg/Hebrew-Mistral-7B" #"mistralai/Mistral-7B-v0.1" #"NousResearch/Meta-Llama-3-8B-Instruct" #"tuned-llama-2-7b"# #"NousResearch/Meta-Llama-3-8B-Instruct" # "unsloth/llama-3-8b-bnb-4bit" #"NousResearch/Meta-Llama-3-8B-Instruct" #"mistralai/Mistral-7B-v0.1"#"mistral-community/Mixtral-8x22B-v0.1"#"mistralai/Mistral-7B-v0.1"#"mistral-community/Mixtral-8x22B-v0.1"
-model = AutoModelForCausalLM.from_pretrained(model_id,
-   quantization_config=quant_config,
-   device_map='auto')#{"": 0})
-#"mistralai/Mistral-7B-v0.1")
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-#"mistralai/Mistral-7B-v0.1")
-
-mistral_7b = Mistral7B(model=model, tokenizer=tokenizer)
-#print(mistral_7b.generate("Write me a joke"))
-
-model_outputs_csv_path = "tuned_llama-3_usage_dataset.csv"
-eval_csv_dict = csv_to_dict(model_outputs_csv_path)
-key = list(eval_csv_dict.keys())[0]
-total_iterations = 2#len(eval_csv_dict[key])//12
-#cmd = "deepeval login --confident-api-key pGKpNnRb9JDf2VwL+UZwokgCXeVPlh9W2Ls/9dNqgDU=" 
-#os.system(cmd)
-#event_id = subprocess.check_output(cmd, shell=True).rstrip()
-llm_cases = []
-json_path = "tests_results.json"
-metric = AnswerRelevancyMetric(threshold=0.5,model=mistral_7b,include_reason=True,async_mode=False,)
-metric2 =  GEval(
-    name="Correctness",
-    criteria="Determine whether the actual output is factually correct based on the expected output.",
-    # NOTE: you can only provide either criteria or evaluation_steps, and not both
-    evaluation_steps=[
-        "Check whether the facts in 'actual output' contradicts any facts in 'expected output'",
-        "You should also heavily penalize omission of detail",
-        "Vague language, or contradicting OPINIONS, are OK"
-    ],
-    evaluation_params=[LLMTestCaseParams.INPUT, LLMTestCaseParams.ACTUAL_OUTPUT],
-    async_mode= False,
-    model = mistral_7b,
-)
-for i in tqdm(range(total_iterations-1), desc="Processing", unit="iterations"):
-# Replace this with the actual output from your LLM application
-#for i in range(len(eval_csv_dict[list(eval_csv_dict.keys())[0]])):
-    
-    
-    #"We offer a 30-day full refund at no extra cost."
-
-    
-    
-    #we do not have context so we will use the reference output as context
-    #print(f"fot the following test we have input : {eval_csv_dict['question'][i]}\n actual output \
-    #      = {eval_csv_dict['actual_output'][i]} \n \
-    #      expected_output={eval_csv_dict['expected_output'][i]}")
-    print(f"\t running test number {i}  ")
-    print("################################")
-    test_case = LLMTestCase(
-        input=eval_csv_dict['question'][i+1],
-        actual_output=eval_csv_dict['actual_output'][i+1],
-        expected_output=eval_csv_dict['expected_output'][i+1],
-        retrieval_context= [eval_csv_dict['expected_output'][i+1]]
-        
+def eval():
+    compute_dtype = getattr(torch, "float16")
+    quant_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=compute_dtype,
+        bnb_4bit_use_double_quant=False,
     )
-    llm_cases.append(test_case)
-    
-    #metric.measure(test_case)
-    #print(metric.score)
-    #print(metric.reason)
 
-    # or evaluate test cases in bulk
-    #temp = evaluate([test_case], [metric,metric2],ignore_errors= True, print_results= True,use_cache=True)
+    #######################
+    ### Load Eval Model ###
+    #######################
+    save_path =os.path.join( os.getcwd(),'eval_test')
+    os.environ["DEEPEVAL_RESULTS_FOLDER"] = save_path
+    
+
+    model_id = "yam-peleg/Hebrew-Mistral-7B" #"mistralai/Mistral-7B-v0.1" # #"mistralai/Mistral-7B-v0.1" #"NousResearch/Meta-Llama-3-8B-Instruct" #"tuned-llama-2-7b"# #"NousResearch/Meta-Llama-3-8B-Instruct" # "unsloth/llama-3-8b-bnb-4bit" #"NousResearch/Meta-Llama-3-8B-Instruct" #"mistralai/Mistral-7B-v0.1"#"mistral-community/Mixtral-8x22B-v0.1"#"mistralai/Mistral-7B-v0.1"#"mistral-community/Mixtral-8x22B-v0.1"
+    model = AutoModelForCausalLM.from_pretrained(model_id,
+       quantization_config=quant_config,
+       device_map='auto')#{"": 0})
+    #"mistralai/Mistral-7B-v0.1")
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    #"mistralai/Mistral-7B-v0.1")
+
+    mistral_7b = Mistral7B(model=model, tokenizer=tokenizer)
+    #print(mistral_7b.generate("Write me a joke"))
+
+    model_outputs_csv_path = "dataset\\usage_shulahn_aruch_dataset.csv"
+    eval_csv_dict = csv_to_dict(model_outputs_csv_path)
+    key = list(eval_csv_dict.keys())[0]
+    total_iterations = 2 #len(eval_csv_dict[key])
+    #cmd = "deepeval login --confident-api-key pGKpNnRb9JDf2VwL+UZwokgCXeVPlh9W2Ls/9dNqgDU=" 
+    #os.system(cmd)
+    #event_id = subprocess.check_output(cmd, shell=True).rstrip()
+    llm_cases = []
+    json_path = "tests_results.json"
+    metric = AnswerRelevancyMetric(threshold=0.5,
+                                   #model=mistral_7b,
+                                   model = "gpt-3.5-turbo",
+                                   include_reason=True,async_mode=True,)
+    metric2 =  GEval(
+        name="Correctness",
+        criteria="Determine whether the actual output is factually correct based on the expected output.",
+        # NOTE: you can only provide either criteria or evaluation_steps, and not both
+        evaluation_steps=[
+            "Check whether the facts in 'actual output' contradicts any facts in 'expected output'",
+            "You should also heavily penalize omission of detail",
+            "Vague language, or contradicting OPINIONS, are OK"
+        ],
+        evaluation_params=[LLMTestCaseParams.INPUT, LLMTestCaseParams.ACTUAL_OUTPUT],
+        async_mode= True,
+        #model = mistral_7b,
+        model = "gpt-3.5-turbo"
+    )
+    for i in tqdm(range(total_iterations-1), desc="Processing", unit="iterations"):
+        print(f"\t running test number {i}  ")
+        print("################################")
+        test_case = LLMTestCase(
+            input=eval_csv_dict['question'][i+1],
+            actual_output=eval_csv_dict['actual_output'][i+1],
+            expected_output=eval_csv_dict['expected_output'][i+1],
+            retrieval_context= [eval_csv_dict['expected_output'][i+1]]
+
+        )
+        llm_cases.append(test_case)
+
+        #metric.measure(test_case)
+        #print(metric.score)
+        #print(metric.reason)
+
+        # or evaluate test cases in bulk
+        #temp = evaluate([test_case], [metric,metric2],ignore_errors= True, print_results= True,use_cache=True)
+        t=1
+        #results.append(temp)
+    dataset = EvaluationDataset(test_cases=llm_cases)
+    #temp=evaluate(dataset,[metric,metric2],ignore_errors= True, use_cache=True)
+    test_results=evaluate(dataset,[metric,metric2],print_results= True,ignore_errors= True, use_cache=True)
+    # we need to convert it into a dict and than save it
+    #We will convert from test to dict and save to yaml
+    all_results =[]
+    for test in test_results:
+        all_results.append(test_result_to_dict(test))
+
+    save_path_yaml = f"test_eval_save_new_{all_results[0]['Answer Relevancy']['evaluation_model']}.yaml"
+    save_list_to_json_yaml(all_results,save_path_yaml)
+
+    filleted_results = [result for result in all_results if   result['Answer Relevancy']['score'] != None or   result['Correctness (GEval)']['score'] > 0 ]
+    no_error_results = []
+    just_successful =[result for result in all_results if result['Answer Relevancy']['is_successful'] or result['Correctness (GEval)']['is_successful']]
+    #no_error_results = [result for result in all_results if "invalid JSON" not in  result['Answer Relevancy']['error'] or "invalid JSON" not in  result['Correctness (GEval)']['error'] ]
+    for result in all_results:
+        if  result['Answer Relevancy']['error'] == None or result['Correctness (GEval)']['error'] ==None:
+            no_error_results.append(result)
+            continue
+        if "invalid JSON" not in  result['Answer Relevancy']['error'] or "invalid JSON" not in  result['Correctness (GEval)']['error']:
+            no_error_results.append(result)
+    filleted_save_path = save_path_yaml.replace('.yaml','_filtered.yaml')
+    save_list_to_json_yaml(filleted_results,filleted_save_path)
     t=1
-    #results.append(temp)
-dataset = EvaluationDataset(test_cases=llm_cases)
-evaluate(dataset,[metric,metric2],ignore_errors= True, use_cache=True)
+if __name__ == "__main__":
+    eval()
 
